65d08e23adcdf89757f276754c1d2ebf97079b88
Javier Kipen <jkipen@itba.edu.ar>
2017-11-04 18:12:36 -0300

    Se agrega nugget allegro

--- Project1/Project1/Controller.h
+++ Project1/Project1/Controller.h
@@ -2,17 +2,14 @@
 #define CONTROLLER_H
 #include <string>
 #include <vector>
-#include "EventData.h"
 
 using namespace std;
 
 class Controller {
 public:
     Controller() {};
-    virtual void parseMouseEvent(EventData *mouseEvent)=0;
-    virtual void parseNetworkEvent(EventData *mouseEvent)=0;
-    virtual void parseKeyboardEvent(EventData *mouseEvent)=0;
-    virtual void parseTimerEvent(EventData *mouseEvent)=0;
+    virtual void parseKeyboardEvent(void *mouseEvent)=0;
+    virtual void parseTimerEvent(void *mouseEvent)=0;
     virtual ~Controller() {};
 };
 
